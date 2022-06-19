dec76a342bd984a0d9a5535177ab676b907a0307
Vladislav13071986 <105044347+Vladislav13071986@users.noreply.github.com>
2022-06-19 15:09:23 +0300

    Add files via upload

---
+++ javascript/sliderConfig.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', () => 
+  			{
+  				new ItcSimpleSlider('#slider-1', 
+  				{
+    				loop: true,
+   					autoplay: true,
+    				interval: 4000,
+    				swipe: true,
+  				});
+			});
